74a1d43efdf1b606aaf05fce4a15e4383c497df7
romain.ren <romain.ren@efrei.net>
2026-01-23 10:23:42 +0100

    [ADD] case class championStats

---
+++ src/main/scala/ChampionStats.scala
@@ -0,0 +1,20 @@
+case class championStats (
+  id : Int,
+  name : String,
+  role: String,
+  difficulty: Int,
+  releaseYear: Int,
+  pickRate: Double,
+  banRate: Double,
+  winRate: Double,
+  gamesPlayed: Int,
+  avgKills: Double,
+  avgDeaths: Double,
+  avgAssists: Double,
+  avgGold: Int,
+  avgDamage: Int,
+  tier: Char,
+  archetype: String
+){
+  id
+}
